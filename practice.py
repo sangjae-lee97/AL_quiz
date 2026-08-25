a018ff0fea0a60c21cf54df53df7a3362704854e
이상재 <tkdwodlaek@naver.com>
2026-08-25 20:49:41 +0900

    함수 정의 하는중

---
+++ practice.py
@@ -0,0 +1,8 @@
+# def compress(r, c, size):
+#     compress = r + c + size
+#     return compress
+
+# answer = compress(3, 5, 6)
+# print(answer)    
+
+split_section = 
---
+++ problem1.py
@@ -0,0 +1,17 @@
+import random
+
+matrix_64x64_random = [
+    "".join(str(random.randint(0, 1)) for _ in range(64))
+    for _ in range(64)
+]
+
+def compress(r, c, size):
+    compress(0,0,64)
+color = data[r][c]
+
+for color in matrix_64x64_random:
+
+
+    
+
+
