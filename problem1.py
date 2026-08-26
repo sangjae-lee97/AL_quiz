bd8db10ef747218865b3755716ddb2ca80938e26
이상재 <tkdwodlaek@naver.com>
2026-08-26 10:14:53 +0900

    함수 선언 완료

--- problem1.py
+++ problem1.py
@@ -6,10 +6,30 @@ matrix_64x64_random = [
 ]
 
 def compress(r, c, size):
-    compress(0,0,64)
-color = data[r][c]
+    half = size // 2
+    split_sections = [(r, c, half), (r, c + half, half), (r + half, c, half), (r + half, c + half, half)]
+    first_color = data[r][c]
+    is_same = True
 
-for color in matrix_64x64_random:
+    for i in range(r, r + size):
+        for j in range(c, c + size):
+            if data[i][j] != first_color:
+                is_same = False
+                break
+        if is_same == False:
+            break
+
+    if is_same == False:
+        print("(", end="")
+        compress(r, c, half)
+        compress(r, c + half, half)
+        compress(r + half, c, half)
+        compress(r + half, c + half, half)
+        print(")", end="")
+    else:
+        print(first_color, end="")
+
+    compress(0,0,N)
 
 
     
